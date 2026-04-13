edcba56997c9d48ce395a0e3b6fc7cbad5231ef1
nishankvarshney07-ai <nishankvarshney07@gmail.com>
2026-04-13 19:48:46 +0530

    new change

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function  App() {
+  const [userData,setUserData] = useState([]);
+  const getData = async ()=>{
+      const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
+      setUserData(response.data);
+  }
   return (
-    <div className='bg-amber-400'  >App</div>
-  )
+    <div className='bg-black h-screen  text-white'>
+    <button onClick={getData} className='bg-green-600 rounded text-white m-4 py-2 px-2 '>Get data</button>
+    </div> 
+  )   
 }
-
 export default  App
