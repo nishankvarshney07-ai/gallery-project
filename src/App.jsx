62d54bda93e5be8e1c3500f134dd16461818cbdc
nishankvarshney07-ai <nishankvarshney07@gmail.com>
2026-04-15 00:31:16 +0530

    adding buttons

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,37 @@ function  App() {
       const response = await axios.get('https://picsum.photos/v2/list?page=2&limit=100')
       setUserData(response.data);
 
-  }
-  useEffect(()=>{
+   }
+   useEffect(()=>{
     getData();  
-  },[index])
+   },[index])
 
   let printuserData = <h3 className='text-gray-500 text-xs absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-semibold'>...loading</h3>
    if (userData.length>0){
-printuserData = userData.map((elem,idx)=>{
+   printuserData = userData.map((elem,idx)=>{
    
-  return <div key={idx}>
+   return <div key={idx}>
     <Card elem={elem} />
-  </div>
+   </div>
 })
   }
   return (
-    <div className='bg-black h-screen   text-white'>
-    <button onClick={getData} className='bg-green-600 rounded flex flex-wrap text-white m-4 py-2 px-2 '>Get data</button>
+    <div className='bg-black h-screen  p-4 overflow-auto text-white'>
+      <div className='flex h-[82%] flex-wrap gap-4 p-2'>
+        {printuserData}
+      </div>
+
+      <div className='flex justify-center gap-6 items-center p-4 '>
+        <button style={{opacity:index==1?0.6:1}} 
+        onClick={()=>{
+          if(index>1){
+            setIndex(index-1)
+            setUserData([])
+          }
+        }}>
+        Prev
+        </button>
+      </div>
     </div> 
   )   
 }
