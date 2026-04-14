b5d8e5459191f6795f08b900c6d6cfe7073bc2f8
nishankvarshney07-ai <nishankvarshney07@gmail.com>
2026-04-14 23:56:44 +0530

    addinf features usinf useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,17 @@ function  App() {
   }
   useEffect(()=>{
     getData();  
-
   },[index])
+
+  let printuserData = <h3 className='text-gray-500 text-xs absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 font-semibold'>...loading</h3>
+   if (userData.length>0){
+printuserData = userData.map((elem,idx)=>{
+   
+  return <div key={idx}>
+    <Card elem={elem} />
+  </div>
+})
+  }
   return (
     <div className='bg-black h-screen   text-white'>
     <button onClick={getData} className='bg-green-600 rounded flex flex-wrap text-white m-4 py-2 px-2 '>Get data</button>
---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Card = (props) => {
+  return (
+    <div>
+        <a href={props.elem.url} target='_blank'>
+
+        </a> 
+    </div>
+  )
+}
+
+export default Card
